083e52e574ee72e0e22248fbedcea053fa44636a
mhmdkaram213-code <mhmdkaram213@gmail.com>
2026-02-01 16:49:48 +0200

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ecommerce.routemisr.com',
+        pathname: '/Route-Academy-products/**',
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/blog',
+        destination: '/blog/web',
+        permanent: true,
+      },
+    ]
+  }
   /* config options here */
 };
 
